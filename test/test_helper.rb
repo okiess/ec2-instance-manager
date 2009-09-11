dc5f64cb51ae1fbade682b5dd53082c64f279982
Oliver Kiessler <kiessler@inceedo.com>
2009-09-11 22:40:10 +0200

    Initial commit to ec2-instance-manager.

---
+++ test/test_helper.rb
@@ -0,0 +1,10 @@
+require 'rubygems'
+require 'test/unit'
+require 'shoulda'
+
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+require 'ec2-instance-manager'
+
+class Test::Unit::TestCase
+end
